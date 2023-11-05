2401a3be3ac9bd93f7eb2dc577e83918b3dcf160
Jawad Rashid <jawadrashid2010@gmail.com>
2023-11-05 19:58:24 +0500

    Added about page

--- index.jsx
+++ index.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <h1>Hello world!</h1>;
+  return <h1>Hello, React Router!</h1>;
 }
+
+function About() {
+  return (
+    <h1>This is Jawad website about page</h1>
+  )
+}
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   </BrowserRouter>
 );
