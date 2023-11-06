039f3b57972bca531af57a9e52e385d1dc27618f
Jawad Rashid <jawadrashid2010@gmail.com>
2023-11-06 23:53:13 +0500

    Added links and routes for host

--- index.jsx
+++ index.jsx
@@ -7,6 +7,7 @@ import Vans from "./pages/Vans/Vans";
 import VanDetail from "./pages/Vans/VanDetails";
 import Layout from "./components/Layout"
 
+import HostLayout from "./pages/Host/HostLayout"
 import Dashboard from "./pages/Host/Dashboard"
 import Income from "./pages/Host/Income"
 import Reviews from "./pages/Host/Reviews"
@@ -22,9 +23,11 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
-          <Route path="/host" element={<Dashboard />} />
-          <Route path="/host/income" element={<Income />} />
-          <Route path="/host/reviews" element={<Reviews />} />
+          <Route path="/host" element={<HostLayout />}>
+            <Route path="/host" element={<Dashboard />} />
+            <Route path="/host/income" element={<Income />} />
+            <Route path="/host/reviews" element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
