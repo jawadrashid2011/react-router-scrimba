e41048d913c2275299c4b005f3681931e7cd4563
Jawad Rashid <jawadrashid2010@gmail.com>
2023-11-06 17:31:19 +0500

    My Version

--- index.jsx
+++ index.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <nav>
+          <Link to="/" className="main-logo">
+            #VANLIFE
+          </Link>
+          <Link to="/about" className="">
+            About
+          </Link>
+        </nav>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
@@ -18,66 +28,5 @@ function App() {
   );
 }
 
-function Home() {
-  return (
-    <>
-      <nav>
-        <Link to="/" className="main-logo">
-          #VANLIFE
-        </Link>
-        <Link to="/about" className="">About</Link>
-      </nav>
-      <div className="home-content">
-        <div className="home-content-text">
-          <h1 className="home-content-main-heading">
-            You got the travel plans, we got the travel vans.
-          </h1>
-          <p className="home-content-paragraph">
-            Add adventure to your life by joining the #vanlife movement.
-            <br /> Rent the perfect van to make your perfect road trip.
-          </p>
-          <button class="van-button">Find your van</button>
-        </div>
-      </div>
-    </>
-  );
-}
-
-function About() {
-  return (
-    <>
-      <nav>
-        <Link to="/" className="main-logo">
-          #VANLIFE
-        </Link>
-        <Link to="/about" className="selected">
-          About
-        </Link>
-      </nav>
-      <div className="about-content">
-        <img src="./images/about_bg.png" />
-        <div className="about-info">
-          <h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
-          <p>
-            Our mission is to enliven your road trip with the perfect travel van
-            rental. Our vans are recertified before each trip to ensure your
-            travel plans can go off without a hitch. (Hitch costs extra 😉)
-            <br />
-            <br />
-            Our team is full of vanlife enthusiasts who know firsthand the magic
-            of touring the world on 4 wheels.
-          </p>
-          <div className="about-vans-info">
-            <h2>
-              Your destination is waiting. <br />
-              Your van is ready.
-            </h2>
-            <button className="about-vans-btn">Explore our vans</button>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-}
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
---
+++ pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <>
+      <div className="home-content">
+        <div className="home-content-text">
+          <h1 className="home-content-main-heading">
+            You got the travel plans, we got the travel vans.
+          </h1>
+          <p className="home-content-paragraph">
+            Add adventure to your life by joining the #vanlife movement.
+            <br /> Rent the perfect van to make your perfect road trip.
+          </p>
+          <button className="van-button">Find your van</button>
+        </div>
+      </div>
+    </>
+  );
+}
