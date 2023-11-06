43c3880020c68a812081cdefdef68d4000bf3d65
Jawad Rashid <jawadrashid2010@gmail.com>
2023-11-06 19:38:34 +0500

    Final Version of vans

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,54 +1,42 @@
-import React from "react";
+import React from "react"
 
 /**
- * Challenge: Fetch and map over the data to display it on
- * the vans page. For an extra challenge, spend time styling
- * it to look like the Figma design.
- *
- * Hints:
- * 1. Use `fetch("/api/vans")` to kick off the request to get the
- *    data from our fake Mirage JS server
- * 2. What React hook would you use to fetch data as soon as the
- *    Vans page loads, and only fetch it the one time?
+ * {
+    * id: "1", 
+    * name: "Modest Explorer", 
+    * price: 60, 
+    * description: "The Modest Explorer is a van designed to get you out of the house and into nature. This beauty is equipped with solar panels, a composting toilet, a water tank and kitchenette. The idea is that you can pack up your home and escape for a weekend or even longer!", 
+    * imageUrl: "https://assets.scrimba.com/advanced-react/react-router/modest-explorer.png", 
+    * type: "simple"
+ * }
  */
 
+
 export default function Vans() {
-  const [vans, setVans] = React.useState([]);
+    const [vans, setVans] = React.useState([])
+    React.useEffect(() => {
+        fetch("/api/vans")
+            .then(res => res.json())
+            .then(data => setVans(data.vans))
+    }, [])
 
-  React.useEffect(() => {
-    fetch("/api/vans")
-      .then((res) => res.json())
-      .then((data) => setVans(data.vans));
-  }, []);
+    const vanElements = vans.map(van => (
+        <div key={van.id} className="van-tile">
+            <img src={van.imageUrl} />
+            <div className="van-info">
+                <h3>{van.name}</h3>
+                <p>${van.price}<span>/day</span></p>
+            </div>
+            <i className={`van-type ${van.type} selected`}>{van.type}</i>
+        </div>
+    ))
 
-  const vansElements = vans.map((van) => {
     return (
-      <div key={van.id}>
-        <img src={van.imageUrl} />
-        <div className="vans-text">
-          <div className="vans-name">{van.name}</div>
-          <div className="vans-price-container">
-            <div className="vans-price">${van.price}</div>
-            <div className="vans-per">/day</div>
-          </div>
+        <div className="van-list-container">
+            <h1>Explore our van options</h1>
+            <div className="van-list">
+                {vanElements}
+            </div>
         </div>
-        <button className={`vans-button ${van.type}`}>{van.type}</button>
-      </div>
-    );
-  });
-
-  return (
-    <div className="vans-page-container">
-      <h1>Explore our vans options</h1>
-      <div className="vans-filters">
-        <div className="vans-filters-buttons">
-          <button>Simple</button>
-          <button>Luxury</button>
-          <button>Rugged</button>
-        </div>
-        <div className="vans-filters-clear">Clear Filters</div>
-      </div>
-      <div className="vans-grid">{vansElements}</div>
-    </div>
-  );
+    )
 }
