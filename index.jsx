49a7c04da292d1bf89487f8e438484643ab53041
Jawad Rashid <jawadrashid2010@gmail.com>
2023-11-06 15:45:46 +0500

    Style nav

--- index.jsx
+++ index.jsx
@@ -5,8 +5,10 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
-    <Link to="/">Home</Link>
-    <Link to="/about">About</Link>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
