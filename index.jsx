c12bdca6ba8540e70cb8746e44cbffc4e9f6c552
Jawad Rashid <jawadrashid2010@gmail.com>
2023-11-07 15:31:32 +0500

    One way to make back button work

--- index.jsx
+++ index.jsx
@@ -1,20 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Vans from "./pages/Vans/Vans";
-import VanDetail from "./pages/Vans/VanDetails";
-import Layout from "./components/Layout"
-
-import HostLayout from "./components/HostLayout"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Vans from "./pages/Vans/Vans"
+import VanDetail from "./pages/Vans/VanDetail"
 import Dashboard from "./pages/Host/Dashboard"
 import Income from "./pages/Host/Income"
 import Reviews from "./pages/Host/Reviews"
+import HostVans from "./pages/Host/HostVans"
+import HostVanDetail from "./pages/Host/HostVanDetail"
+import Layout from "./components/Layout"
+import HostLayout from "./components/HostLayout"
 
-import "./server";
-import HostVans from "./pages/Host/HostVans";
-import HostVanDetail from "./pages/Host/HostVanDetail";
+import "./server"
 
 function App() {
   return (
@@ -25,18 +24,22 @@ function App() {
           <Route path="about" element={<About />} />
           <Route path="vans" element={<Vans />} />
           <Route path="vans/:id" element={<VanDetail />} />
-
+          
           <Route path="host" element={<HostLayout />}>
             <Route index element={<Dashboard />} />
-            <Route path="vans" element={<HostVans />} />
-            <Route path="vans/:id" element={<HostVanDetail />} />
             <Route path="income" element={<Income />} />
             <Route path="reviews" element={<Reviews />} />
+            <Route path="vans" element={<Outlet />}>
+              <Route index element={<HostVans />} />
+              <Route path=":id" element={<HostVanDetail />} />
+            </Route>
           </Route>
         </Route>
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(<App />);
